6334c4a9c66de7d6d9f3817fa912504c3d59ba8f
Anup Bhandari <anup.b@HOBB.COM>
2023-09-27 10:20:42 +0530

    Removed dead code

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import * as THREE from './node_modules/three/src/Three.js'
 import * as ENGINE from './engine/Engine.js'
 import {GLTFLoader} from './node_modules/three/examples/jsm/loaders/GLTFLoader.js'
 
-const COLORS = [{r:0, g:139, b:139}, {r:100, g:149, b:237}, {r:205, g:92, b:92}, {r:95, g:158, b:160}, {r:255, g:99, b:71}, 
-{r:220, g:20, b:60}, {r:255, g:255, b:0}, {r:128, g:128, b:0}, {r:0, g:128, b:128}, {r:238, g:130, b:238}, {r:165, g:42, b:42}, 
-{r:255, g:215, b:0}]
 const TEXTURE_PATHS = ['assets/images/1.png', 'assets/images/2.png', 'assets/images/3.png', 'assets/images/4.png', 'assets/images/5.png', 'assets/images/6.png']
 const TEXTURES = []
 const TEXTURE_BASE_KEY = 'texture'
@@ -17,28 +14,7 @@ let yrot = 0
 let model
 loadAssets()
 
-window.onload = () => 
-{
-    fillupTextures(document.getElementById('panel-texture-body'))
-}
-
-function fillupColors(colorContainer)
-{
-    for(let i=0; i<COLORS.length; i++)
-    {
-        let divElement = document.createElement('div')
-        divElement.className = 'panel-content'
-        divElement.style.backgroundColor = 'rgb('+COLORS[i].r+', '+COLORS[i].g+', '+COLORS[i].b+')'
-        divElement.addEventListener('click', ()=>{onColorClick(i)})
-        colorContainer.appendChild(divElement)
-    }
-}
-
-function onColorClick(index)
-{
-    if (model != undefined)
-        model.applyColor(new THREE.Color(COLORS[index].r/255, COLORS[index].g/255, COLORS[index].b/255))
-}
+window.onload = () => { fillupTextures(document.getElementById('panel-texture-body')) }
 
 function fillupTextures(textureContainer)
 {
